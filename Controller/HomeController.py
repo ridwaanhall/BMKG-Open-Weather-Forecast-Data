6ecc32c0aa364bc1b637f79f7ab38e7b06c88cb8
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-15 12:15:39 +0000

    Add to read json data in home.json

---
+++ Controller/HomeController.py
@@ -0,0 +1,16 @@
+import json, xmltodict
+from flask import Response
+
+class HomeController:
+
+  @staticmethod
+  def load_home_json():
+    with open('Data/home.json', 'r') as json_file:
+      data = json.load(json_file)
+    return data
+
+  @staticmethod
+  def load_home_xml():
+    data = HomeController.load_home_json()
+    xml_data = xmltodict.unparse({"data": data}, pretty=True)
+    return Response(xml_data, content_type='text/xml')
